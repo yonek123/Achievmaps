b5542437848ec54b88e4af7038e2da845f36493a
yonek123 <46713174+yonek123@users.noreply.github.com>
2022-11-16 13:00:47 +0100

    Update kk44377.sql

--- sql/kk44377.sql
+++ sql/kk44377.sql
@@ -26,14 +26,14 @@ CREATE TABLE IF NOT EXISTS `AchievementList` (
   `Latitude` text,
   `Longitude` text,
   `Points` int(11) NOT NULL,
-  `OpenTime` time NOT NULL,
-  `CloseTime` time NOT NULL,
-  `OpenTimeTomorrow` time NOT NULL,
-  `CloseTimeTomorrow` time NOT NULL,
-  `OpenTimeD3` time NOT NULL,
-  `CloseTimeD3` time NOT NULL,
-  `ViewTimeMin` time NOT NULL,
-  `ViewTimeOpt` time NOT NULL,
+  `OpenTime` time,
+  `CloseTime` time,
+  `OpenTimeTomorrow` time,
+  `CloseTimeTomorrow` time,
+  `OpenTimeD3` time,
+  `CloseTimeD3` time,
+  `ViewTime` time,
+  `Requirements` int(11) NOT NULL,
   `Tags` text NOT NULL,
   PRIMARY KEY (`Achievementid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -325,13 +325,14 @@ CREATE TABLE IF NOT EXISTS `user12Achievements` (
 -- Zrzut danych tabeli `UsersAchievements`
 --
 
-INSERT INTO `AchievementList` (`AchievementName`, `Description`, `Latitude`, `Longitude`, `Points`, `OpenTime`, `CloseTime`, `OpenTimeTomorrow`, `CloseTimeTomorrow`, `OpenTimeD3`, `CloseTimeD3`, `ViewTimeMin`, `ViewTimeOpt`, `ViewTimeMax`, `Tags`) VALUES
-('Baszta Panieńska', 'Odwiedź Basztę Panieńską, zwaną też Basztą Siedmiu Płaszczy w Szczecinie.', '53.42631352116791', '14.562885502712085', '10', '10:00:00.00', '17:00:00.00', '10:00:00.00', '17:00:00.00', '24:00:00.00', '24:00:00.00', '00:00:00.00', '00:30:00.00', 'Europa;Polska;Architektura'),
-('Brama Królewska', 'Odwiedź Bramę Królewską w Szczecinie i napij się przepysznej czekolady Wedel.', '53.42832774717961', '14.556718555956685', '50', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:00:00.00', '00:30:00.00', 'Europa;Polska;Architektura'),
-('Brama Portowa', 'Odwiedź Bramę Portową w Szczecinie. Wewnątrz zobaczyć można przedstawienia Teatru Kameralnego. Odwiedź ich stronę internetową po szczegóły.', '53.424932256502316', '14.550359100137214', '50', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:00:00.00', '00:10:00.00', 'Europa;Polska;Architektura'),
-('Kamienica Loitzów', 'Zobacz przepiękną Kamienicę Loitzów w Szczecinie.', '53.425233964007774', '14.559322938759125', '20', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:00:00.00', '00:10:00.00', 'Europa;Polska;Architektura'),
-('Kamienica Augusta Mintza', 'Zobacz Kamienicę Augusta Mintza w Szczecinie i wejdź do umieszczonej w nim zabytkowej apteki.', '53.423827139607404', '14.559419472358165', '10', '24:00:00.00', '24:00:00.00', '24:00:00.00', '24:00:00.00', '24:00:00.00', '24:00:00.00', '00:00:00.00', '01:00:00.00', 'Europa;Polska;Architektura'),
+INSERT INTO `AchievementList` (`AchievementName`, `Description`, `Latitude`, `Longitude`, `Points`, `OpenTime`, `CloseTime`, `OpenTimeTomorrow`, `CloseTimeTomorrow`, `OpenTimeD3`, `CloseTimeD3`, `ViewTime`, `Requirements`, `Tags`) VALUES
+('Baszta Panieńska', 'Odwiedź Basztę Panieńską, zwaną też Basztą Siedmiu Płaszczy w Szczecinie.', '53.42631352116791', '14.562885502712085', '10', '10:00:00.00', '17:00:00.00', '10:00:00.00', '17:00:00.00', NULL, NULL, '00:30:00.00', '1', 'Europa;Polska;Architektura'),
+('Brama Królewska', 'Odwiedź Bramę Królewską w Szczecinie i napij się przepysznej czekolady Wedel.', '53.42832774717961', '14.556718555956685', '50', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:30:00.00', '1', 'Europa;Polska;Architektura'),
+('Brama Portowa', 'Odwiedź Bramę Portową w Szczecinie. Wewnątrz zobaczyć można przedstawienia Teatru Kameralnego. Odwiedź ich stronę internetową po szczegóły.', '53.424932256502316', '14.550359100137214', '50', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:10:00.00', '1', 'Europa;Polska;Architektura'),
+('Kamienica Loitzów', 'Zobacz przepiękną Kamienicę Loitzów w Szczecinie.', '53.425233964007774', '14.559322938759125', '20', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:00:00.00', '24:00:00.00', '00:10:00.00', '1', 'Europa;Polska;Architektura'),
+('Kamienica Augusta Mintza', 'Zobacz Kamienicę Augusta Mintza w Szczecinie i wejdź do umieszczonej w nim zabytkowej apteki.', '53.423827139607404', '14.559419472358165', '10', NULL, NULL, NULL, NULL, NULL, NULL, '01:00:00.00', '1', 'Europa;Polska;Architektura'),
 
+('Stare Miasto', 'Starsze niż twoja stara', NULL, NULL, '300', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '10', 'Zbiorowe;Architektura'),
 ('Wały Chrobrego', 'Odwiedź Wały Chrobrego w Szczecin, Zachodniopomorskie, Polska', '53.429902750176595', '14.5650708160754', '10', '00:00:00.00', '24:00:00.00', '00:00:00.00', '00:30:00.00', 'Europa;Polska;Architektura'),
 ('Brama Portowa', 'Odwiedź Brama Portowa w Szczecin, Zachodniopomorskie, Polska', '53.4250537242813', '14.55023035410361', '10', '00:00:00.00', '24:00:00.00', '00:00:00.00', '00:30:00.00', 'Europa;Polska;Architektura');
 
